52f6fe285b23a13f91cb999ec33d84343e9df5a8
shaff2105 <104710815+shaff2105@users.noreply.github.com>
2022-12-31 13:44:14 +0700

    Add files via upload

---
+++ uas No.5/uaspboinfabs/src/Pet.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author oktor
+ */
+public interface Pet {
+ public String getName();
+ public void setName(String name);
+ public void play();
+
+}
