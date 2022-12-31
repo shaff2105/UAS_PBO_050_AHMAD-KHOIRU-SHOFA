52d92bb22287b2bb78684f13f26f4781813fdd64
shaff2105 <104710815+shaff2105@users.noreply.github.com>
2022-12-31 13:29:44 +0700

    Add files via upload

---
+++ Cat.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author oktor
+ */
+public class Cat extends Animal implements Pet {
+ private String catName;
+ public Cat()
+ {
+ this("");
+ }
+ public Cat(String name)
+ {
+ super(4);
+ catName=name;
+ }
+ // @Override
+ public void eat() {
+ System.out.println("Cat eat fishes");
+ }
+ public String getName() {
+ return catName;
+ }
+ public void setName(String name) {
+ catName=name;
+ }
+ public void play() {
+ throw new UnsupportedOperationException("Not supported yet.");
+ }
+}
